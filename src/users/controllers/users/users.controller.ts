65227a31d013ef59ed1dcb319a637bbb4ae51688
Almir Muminovic <projectalmir@gmail.com>
2023-05-24 23:01:46 +0200

    Creating basic connection between controllers and services

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from 'src/users/services/users/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private readonly usersService: UsersService){
+
+    }
+
+    @Get()
+    async getUsers(){
+        try {
+            const users = await this.usersService.getUsers()
+            return users
+        } catch (e) {
+            console.log(e)
+        }
+
+    }
+
+    @Post()
+    async createUser(@Body() userData: any){
+        try {
+            console.log(userData)
+            const user = await this.usersService.createUser()
+            return user
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+
+}
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(){}
+
+    getUsers(){
+        // db connection
+        const users = "call repository"
+        return users
+    }
+
+    createUser(){
+        //db connection
+        const user = "call repository create"
+        return user;
+    }
+
+}

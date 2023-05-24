2c58758dc6aec6835e6d02825fd276ce9983d749
Almir Muminovic <projectalmir@gmail.com>
2023-05-24 16:06:44 +0200

    Simple configuration with postgres as database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres', 
+    host: 'localhost', 
+    port: 5432, 
+    username: 'almirmuminovic', 
+    password: 'pass123', 
+    database: 'typeorm', 
+    entities: [],
+    synchronize: true, 
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
